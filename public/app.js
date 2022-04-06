4dfd8c576d960988c6659fba7bf7323b82c16326
Your Namegit config --global user.name Your <you@example.com>
2022-04-06 18:34:41 -0400

    First Draft

--- public/app.js
+++ public/app.js
@@ -1,10 +1,19 @@
-// create map
 
+// Create map:                                                       
+const myMap = L.map('map', {
+    center: [48.868672, 2.342130],
+    zoom: 12,
+});
 
-// add openstreetmap tiles
+// Add OpenStreetMap tiles:
+L.tileLayer('https://.tile.openstreetmap.org///.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: '15',
+}).addTo(myMap)
 
-
-// create and main add geolocation marker
+// Create and add a geolocation marker:
+const marker = L.marker([48.87007, 2.346453])
+marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
 
 
 // draw the 2nd arrondissement
