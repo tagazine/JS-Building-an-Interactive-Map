1a1357bb83964513750a29bb4d67ae3a24976c38
Your Namegit config --global user.name Your <you@example.com>
2022-04-06 18:47:16 -0400

    Fixes

--- public/app.js
+++ public/app.js
@@ -25,5 +25,24 @@ var polygon = L.polygon([
 ]).addTo(map);
 
 // create red pin marker
+const rS = L.marker([48.866200610611926, 2.352236247419453],{icon: redPin}).bindPopup('RÃ©aumur-SÃ©bastopol')
 
 // metro station markers
+
+const sSD = L.marker([48.869531786321566, 2.3528590208055196]).bindPopup(
+  "Strasbourg-Saint-Denis"
+);
+const sentier = L.marker([48.8673721067762, 2.347107922912739]).bindPopup(
+  "Sentier"
+);
+const bourse = L.marker([48.86868503971672, 2.3412285142058167]).bindPopup(
+  "Bourse"
+);
+const qS = L.marker([48.869560129483226, 2.3358638645569543]).bindPopup(
+  "Quatre Septembre"
+);
+const gB = L.marker([48.871282159004856, 2.3434818588892714]).bindPopup(
+  "Grands Boulevards"
+);
+
+const stations = L.layerGroup([rS, sSD, sentier, bourse, qS, gB]).addTo(myMap);
